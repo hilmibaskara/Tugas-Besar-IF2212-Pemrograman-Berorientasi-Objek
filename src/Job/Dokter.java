f604036edaa61b0495c54bb0ce156795e9599185
dwicakradaniel <110537201+DDaniel-7@users.noreply.github.com>
2023-04-22 19:47:52 +0700

    Add files via upload

---
+++ src/Job/Dokter.java
@@ -0,0 +1,14 @@
+public class Dokter implements Job {
+    private String namajob;
+    private int gaji;
+    public Dokter() {
+        this.namajob = namajob;
+        this.gaji = gaji;
+    }
+    public String getNamaPekerjaan(){
+        return namajob = "Dokter";
+    }
+    public int getGaji(){
+        return gaji = 50;
+    }
+}
---
+++ src/Job/Programmer.java
@@ -0,0 +1,14 @@
+public class Programmer implements Job{
+    private String namajob;
+    private int gaji;
+    public Programmer() {
+        this.namajob = namajob;
+        this.gaji = gaji;
+    }
+    public String getNamaPekerjaan(){
+        return namajob = "Programmer";
+    }
+    public int getGaji(){
+        return gaji = 45;
+    }
+}
