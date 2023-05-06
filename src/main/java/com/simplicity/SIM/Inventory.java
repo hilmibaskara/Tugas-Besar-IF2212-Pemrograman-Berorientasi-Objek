395a75f817aecba7496e1950bcfe8b4b2ddc45ec
darrendoang <16521063@mahasiswa.itb.ac.id>
2023-05-06 20:00:42 +0700

    Inventory Functionality

--- src/main/java/com/simplicity/SIM/Inventory.java
+++ src/main/java/com/simplicity/SIM/Inventory.java
@@ -9,7 +9,7 @@ public class Inventory {
     private ArrayList<Objek> objects = new ArrayList<>();
     private boolean status;
     private int capacity;
-
+    Scanner scan = new Scanner(System.in);
     public Inventory(SIM owner) {
         this.owner = owner;
         this.capacity = 100;
@@ -68,6 +68,156 @@ public class Inventory {
             System.out.println(i + ". " + objek.getNama() + " (" + amount + ")");
             i++;
         }
+        if (i == 1) {
+            System.out.println("Inventory kosong");
+        }
+        else{
+            System.out.println("List Aksi:");
+            System.out.println("1. Gunakan Objek");
+            System.out.println("2. Leave");
+
+            System.out.println("Masukan nomor aksi yang diinginkan:");
+            int choice = scan.nextInt();
+            if (choice ==1 ){
+                System.out.println("Pilih nomor objek yang ingin digunakan: ");
+                int pasang = scan.nextInt();
+                Objek objek = (Objek) objekCountMap.keySet().toArray()[pasang - 1];
+                if (objek instanceof ObjekNonMakanan)
+                {
+                    if (pasang> 0 && pasang <= i-1){
+                        if(objek instanceof ObjekNonMakanan){
+                            System.out.printf("masukkan koordinat x: ");
+                            int xpasang = scan.nextInt();
+                            scan.nextLine();
+                            System.out.printf("masukkan koordinat y: ");
+                            int ypasang = scan.nextInt();
+                            scan.nextLine();
+                            if(objek.getNama().equals("Meja dan Kursi")){
+                                MejaDanKursi mejakursi = new MejaDanKursi();
+                                boolean stats = owner.pasangBarang(mejakursi,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                                
+                            }
+                            else if(objek.getNama().equals("Kompor Listrik")){
+                                KomporListrik komporlistrik = new KomporListrik();
+                                boolean stats = owner.pasangBarang(komporlistrik,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                            else if(objek.getNama().equals("Kompor Gas")){
+                                KomporGas komporgas = new KomporGas();
+                                boolean stats = owner.pasangBarang(komporgas,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                            else if(objek.getNama().equals("Toilet")){
+                                Toilet toilet = new Toilet();
+                                boolean stats = owner.pasangBarang(toilet,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                            else if(objek.getNama().equals("Jam")){
+                                Jam jam = new Jam();
+                                boolean stats = owner.pasangBarang(jam,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                            else if(objek.getNama().equals("Kasur Single")){
+                                KasurSingle kasursingle = new KasurSingle();
+                                boolean stats = owner.pasangBarang(kasursingle,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                            else if(objek.getNama().equals("Kasur King Size")){
+                                KasurKingSize kasurkingsize = new KasurKingSize();
+                                boolean stats = owner.pasangBarang(kasurkingsize,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                            else if(objek.getNama().equals("Kasur Queen Size")){
+                                KasurQueenSize kasurqueen = new KasurQueenSize();
+                                boolean stats = owner.pasangBarang(kasurqueen,xpasang,ypasang);
+                                if (stats){
+                                    removeObject(objek);
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        System.out.println("command salah");
+                    }
+                }
+        
+                else if (objek instanceof Masakan)
+                {
+                    owner.makan((ObjekMakanan)objek);
+                }
+                else {
+                    System.out.println("1. makan");
+                    System.out.println("2. masak");
+                    System.out.println("Pilih aksi yang ingin dilakukan: ");
+                    int pilihan = scan.nextInt();
+
+                    // System.out.println("3. buang");
+                    if (pilihan==1){
+                        owner.makan((ObjekMakanan)objek);
+                    }
+                    else if (pilihan == 2 ){
+                        System.out.println("list menu masakan");
+                        System.out.println("1. nasi ayam");
+                        System.out.println("2. nasi kari");
+                        System.out.println("3. kacang");
+                        System.out.println("4. tumis sayur");
+                        System.out.println("5. bistik");
+                        System.out.printf("masukkan nomor masakan yang dipilih: ");
+                        int masakan = scan.nextInt();
+                      
+                        
+                        switch (masakan) {
+                            case 1:
+                                NasiAyam nasiayam = new NasiAyam();
+                                owner.masak(nasiayam);
+                                break;
+                            case 2:
+                                NasiKari nasikari = new NasiKari();
+                                owner.masak(nasikari);
+                                break;
+                            case 3:
+                                SusuKacang susukacang = new SusuKacang();
+                                owner.masak(susukacang);
+                                break;
+                            case 4:
+                                TumisSayur tumissayur = new TumisSayur();
+                                owner.masak(tumissayur);
+                                break;
+                            case 5:
+                                Bistik bistik = new Bistik();
+                                owner.masak(bistik);
+                                break;
+                                default:
+                                System.out.println("command salah");
+                                break;
+                        }
+             
+                    }
+                    else{
+                        System.out.println("pilihan salah salah");
+                    }
+                    
+                }
+            }
+        }
+
+        
+
     }
 
     public int getObjectNumber(){
@@ -87,5 +237,5 @@ public class Inventory {
         }
         return count;
     }
-    
+
 }
